97e1f6be1f935d0a4963ac3e1bd2b3985da974df
Ben Grynhaus <bengr@microsoft.com>
2019-03-14 13:18:38 +0200

    Allow modifying props in custom render function.
    Note that this is only a partial replacement for Fabric's pattern,
    since we still want to abstract away JSX from consumers,
    and Angular doesn't have first-class support for creating templates in TypeScript.

--- libs/core/src/lib/components/wrapper-component.ts
+++ libs/core/src/lib/components/wrapper-component.ts
@@ -5,48 +5,30 @@
 import {
   AfterViewInit,
   ChangeDetectorRef,
-  ComponentFactoryResolver,
-  ComponentRef,
   ElementRef,
-  Injector,
   Input,
   NgZone,
   OnChanges,
   Renderer2,
   SimpleChanges,
-  TemplateRef,
-  Type,
+  AfterContentInit,
 } from '@angular/core';
 import classnames from 'classnames';
 import toStyle from 'css-to-style';
 import stylenames, { StyleObject } from 'stylenames';
+
 import { Many } from '../declarations/many';
 import { ReactContentProps } from '../renderer/react-content';
 import { isReactNode } from '../renderer/react-node';
 import { isReactRendererData } from '../renderer/renderer';
-import { createComponentRenderer, createHtmlRenderer, createTemplateRenderer } from '../renderer/renderprop-helpers';
 import { toObject } from '../utils/object/to-object';
 import { afterRenderFinished } from '../utils/render/render-delay';
-import { unreachable } from '../utils/types/unreachable';
+import { InputRendererOptions, JsxRenderFunc, createInputJsxRenderer, createRenderPropHandler } from './render-props';
 
 // Forbidden attributes are still ignored, since they may be set from the wrapper components themselves (forbidden is only applied for users of the wrapper components)
 const ignoredAttributeMatchers = [/^_?ng-?.*/, /^style$/, /^class$/];
 
 const ngClassRegExp = /^ng-/;
-
-export interface RenderComponentOptions<TContext extends object> {
-  readonly componentType: Type<TContext>;
-  readonly factoryResolver: ComponentFactoryResolver;
-  readonly injector: Injector;
-}
-
-export type InputRendererOptions<TContext extends object> =
-  | TemplateRef<TContext>
-  | ((context: TContext) => HTMLElement)
-  | ComponentRef<TContext>
-  | RenderComponentOptions<TContext>;
-
-export type JsxRenderFunc<TContext> = (context: TContext) => JSX.Element;
 
 export type ContentClassValue = string[] | Set<string> | { [klass: string]: any };
 export type ContentStyleValue = string | StyleObject;
@@ -77,7 +59,7 @@ const defaultWrapperComponentOptions: WrapperComponentOptions = {
  * Simplifies some of the handling around passing down props and CSS styling on the host component.
  */
 // NOTE: TProps is not used at the moment, but a preparation for a potential future change.
-export abstract class ReactWrapperComponent<TProps extends {}> implements AfterViewInit, OnChanges {
+export abstract class ReactWrapperComponent<TProps extends {}> implements AfterContentInit, AfterViewInit, OnChanges {
   private _contentClass: Many<ContentClassValue>;
   private _contentStyle: ContentStyleValue;
 
@@ -185,7 +167,7 @@ export abstract class ReactWrapperComponent<TProps extends {}> implements AfterV
 
   /**
    * Create an JSX renderer for an `@Input` property.
-   * @param input The input property
+   * @param input The input property.
    * @param additionalProps optional additional props to pass to the `ReactContent` object that will render the content.
    */
   protected createInputJsxRenderer<TContext extends object>(
@@ -200,31 +182,7 @@ export abstract class ReactWrapperComponent<TProps extends {}> implements AfterV
       throw new Error('To create an input JSX renderer you must pass an NgZone to the constructor.');
     }
 
-    if (input instanceof TemplateRef) {
-      const templateRenderer = createTemplateRenderer(input, this._ngZone, additionalProps);
-      return (context: TContext) => templateRenderer.render(context);
-    }
-
-    if (input instanceof ComponentRef) {
-      const componentRenderer = createComponentRenderer(input, additionalProps);
-      return (context: TContext) => componentRenderer.render(context);
-    }
-
-    if (input instanceof Function) {
-      const htmlRenderer = createHtmlRenderer(input, additionalProps);
-      return (context: TContext) => htmlRenderer.render(context);
-    }
-
-    if (typeof input === 'object') {
-      const { componentType, factoryResolver, injector } = input;
-      const componentFactory = factoryResolver.resolveComponentFactory(componentType);
-      const componentRef = componentFactory.create(injector);
-
-      // Call the function again with the created ComponentRef<TContext>
-      return this.createInputJsxRenderer(componentRef, additionalProps);
-    }
-
-    unreachable(input);
+    return createInputJsxRenderer(input, this._ngZone, additionalProps);
   }
 
   /**
@@ -233,24 +191,14 @@ export abstract class ReactWrapperComponent<TProps extends {}> implements AfterV
    * @param jsxRenderer an optional renderer to use.
    * @param additionalProps optional additional props to pass to the `ReactContent` object that will render the content.
    */
-  protected createRenderPropHandler<TProps extends object>(
-    renderInputValue: InputRendererOptions<TProps>,
+  protected createRenderPropHandler<TRenderProps extends object>(
+    renderInputValue: InputRendererOptions<TRenderProps>,
     options?: {
-      jsxRenderer?: JsxRenderFunc<TProps>;
+      jsxRenderer?: JsxRenderFunc<TRenderProps>;
       additionalProps?: ReactContentProps;
     }
-  ): (props?: TProps, defaultRender?: JsxRenderFunc<TProps>) => JSX.Element | null {
-    const renderer =
-      (options && options.jsxRenderer) ||
-      this.createInputJsxRenderer(renderInputValue, options && options.additionalProps);
-
-    return (props?: TProps, defaultRender?: JsxRenderFunc<TProps>) => {
-      if (!renderInputValue) {
-        return typeof defaultRender === 'function' ? defaultRender(props) : null;
-      }
-
-      return renderer(props);
-    };
+  ): (props?: TRenderProps, defaultRender?: JsxRenderFunc<TRenderProps>) => JSX.Element | null {
+    return createRenderPropHandler(renderInputValue, this._ngZone, options);
   }
 
   private _passAttributesAsProps() {
@@ -299,10 +247,10 @@ export abstract class ReactWrapperComponent<TProps extends {}> implements AfterV
   }
 
   private _setHostDisplay() {
-    const nativeElement: HTMLElement = this.elementRef.nativeElement;
+    const nativeElement = this.elementRef.nativeElement;
 
     // We want to wait until child elements are rendered
-    setTimeout(() => {
+    afterRenderFinished(() => {
       if (nativeElement.firstElementChild) {
         const rootChildDisplay = getComputedStyle(nativeElement.firstElementChild).display;
         nativeElement.style.display = rootChildDisplay;
